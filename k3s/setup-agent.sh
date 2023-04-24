eb65d75336108109c8d98fa55ec831ab2374617b
Alan White <alan@whitemail.net>
2023-04-24 16:46:58 +0100

    agent script

---
+++ k3s/setup-agent.sh
@@ -0,0 +1,41 @@
+SYS_DIR="/data/system"
+APP_DIR="/data/apps1"
+K3S_VERSION="v1.25.4+k3s1"
+
+# nodefs
+#
+KUBELET_DIR="${SYS_DIR}/kubelet"
+sudo mkdir -p "${KUBELET_DIR}"
+
+# imagefs: containerd has a root and state directory
+#
+# - https://github.com/containerd/containerd/blob/main/docs/ops.md#base-configuration
+#
+# containerd root -> /var/lib/rancher/k3s/agent/containerd
+#
+CONTAINERD_ROOT_DIR_OLD="/var/lib/rancher/k3s/agent"
+CONTAINERD_ROOT_DIR_NEW="${SYS_DIR}/containerd-root/containerd"
+sudo mkdir -p "${CONTAINERD_ROOT_DIR_OLD}"
+sudo mkdir -p "${CONTAINERD_ROOT_DIR_NEW}"
+sudo ln -s "${CONTAINERD_ROOT_DIR_NEW}" "${CONTAINERD_ROOT_DIR_OLD}"
+
+# containerd state -> /run/k3s/containerd
+#
+CONTAINERD_STATE_DIR_OLD="/run/k3s"
+CONTAINERD_STATE_DIR_NEW="${SYS_DIR}/containerd-state/containerd"
+sudo mkdir -p "${CONTAINERD_STATE_DIR_OLD}"
+sudo mkdir -p "${CONTAINERD_STATE_DIR_NEW}"
+sudo ln -s "${CONTAINERD_STATE_DIR_NEW}" "${CONTAINERD_STATE_DIR_OLD}"
+
+# pvs -> /var/lib/rancher/k3s/storage
+#
+PV_DIR_OLD="/var/lib/rancher/k3s"
+PV_DIR_NEW="${APP_DIR}/local-path-provisioner/storage"
+sudo mkdir -p "${PV_DIR_OLD}"
+sudo mkdir -p "${PV_DIR_NEW}"
+sudo ln -s "${PV_DIR_NEW}" "${PV_DIR_OLD}"
+
+# =======
+# Install
+# =======
+curl -sfL https://get.k3s.io | K3S_TOKEN=fredbloggs INSTALL_K3S_EXEC="agent --kubelet-arg "root-dir=$KUBELET_DIR"" sh -s - --server https://192.168.178.201:6443
